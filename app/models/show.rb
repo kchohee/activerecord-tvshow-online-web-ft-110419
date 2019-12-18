4e62beef1b8ead99d58b7d4854b3ebbd293d091d
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-12-18 02:21:23 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,22 +1,22 @@
 class Show < ActiveRecord::Base
   def Show::highest_rating
     self.maximum("rating")
-  end  
+  end
   def Show::most_popular_show
     self.order(rating: highest_rating)
   end
   def Show::lowest_rating
     self.minimum(:rating)
-  end  
+  end
   def self.least_popular_show
     self.find_by(rating: lowest_rating)
-  end  
-  def Show::ratings_sum 
+  end
+  def Show::ratings_sum
     self.sum(:rating)
   end
   def Show::popular_shows
     self.where("rating > 5")
-  end  
+  end
   def Show::shows_by_alphabetical_order
     self.order(:name)
   end
